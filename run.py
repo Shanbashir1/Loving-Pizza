0596d16ed1ae2be99b83c9424a405d156e9cb28f
Shanbashir1 <shanbashir1@hotmail.com>
2022-09-20 19:27:46 +0000

    Added function for user to select pizza sizes, 4 options are displayed to the user, using prettytable format, the user is also prompted with options to select from

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 from decimal import *
 from art import tprint
-from beautifultable import BeautifulTable
+from prettytable import PrettyTable
 from colorama import Fore, Style
 
 """
@@ -28,7 +28,7 @@ SHEET = GSPREAD_CLIENT.open('love_pizza')
 order_str = SHEET.worksheet('order_list')
 data = order_str.get_all_values()
 type_list = order_str.col_values(1)[1:]  
-name_list = order_str.col_values(3)[1:]  
+size_list = order_str.col_values(3)[1:]  
 topping_list = order_str.col_values(4)[1:]  
 pizza_list = order_str.col_values(2)[1:]  
 price_list = order_str.col_values(5)[1:]  
@@ -39,12 +39,15 @@ Data to be returned to worksheet, with customer order informatio
 customer = SHEET.worksheet('order')
 # Defined variables 
 customer_details = []
+prices = []
 
 """
 Defining the main functions 
 """
 def main():
     customer_name()
+    size_names()
+    pizza_size()
 
 """
 Using art format to print a Welcome Logo
@@ -84,7 +87,62 @@ a valid name" + Style.RESET_ALL)
         break
     return customer_name.first, customer_name.surname
 
-   
-   
+"""
+Options for the user to select the size of pizza they require
+The Price is listed and displayed for the user 
+"""
+
+def size_names():
+    """
+    Creates a prettytable for user to select pizza sizes and records data on to worksheet
+    """
+    print("\nPlease Select the Pizza Size you require?")
+    time.sleep(1)  
+    size_name = []
+    for siz_name in size_list:
+        size_name.append(siz_name)
+    num = []
+    for i in range(1, 5):
+        num.append(i)
+
+    size_names.names = dict(zip(num, size_name))
+
+    size_table = PrettyTable()
+    size_table.field_names = num
+    size_table.add_row(size_name)
+    print(size_table)
+    return size_name
+"""
+Prompts user to select options from previous function relating to pizza size. 
+The user is then displayed with their choice
+"""
+def pizza_size():
+    while True:
+        pizza_size.piz = input(Fore.BLUE + "\nPlease select the size of the Pizza you require? \
+1) Small 2) Medium 3) Large 4) Xlarge \n" + Style.RESET_ALL).lower()
+        if pizza_size.piz == "1":
+            customer_details.append("Small")
+            print("\nYou selected SMALL, approx 3-4 slices\n")
+            prices.append(7.50)
+            break
+        elif pizza_size.piz == "2":
+            customer_details.append("Medium")
+            print("\nYou selected MEDIUM, approx 5-6 slices")
+            prices.append(8.50)
+            break
+        elif pizza_size.piz == "3":
+            customer_details.append("Large")
+            print("\nYou selected LARGE, approx 7-8 slices")
+            prices.append(12.50)
+            break
+        elif pizza_size.piz == "4":
+            customer_details.append("XLarge")
+            print("\nYou selected XLARGE, approx 9-10 slices")
+            prices.append(15.50)
+            break
+        else:
+            print(Fore.RED + "Please type 1, 2, 3 or 4" + Style.RESET_ALL)
+            continue
+        return pizza_size
    
 main()
