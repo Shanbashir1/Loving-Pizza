b9bef1c6b0d722cc43d8da5d5837eca659253546
Shanbashir1 <shanbashir1@hotmail.com>
2022-09-20 09:55:38 +0000

    Initial Commit, installing required functions and linking files

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+import sys
+import time
+import random
+from datetime import date, datetime
+import gspread
+from google.oauth2.service_account import Credentials
+from decimal import *
+from art import tprint
+from beautifultable import BeautifulTable
+from colorama import Fore, Style
+
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_pizza')
